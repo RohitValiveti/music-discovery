0877e11a803e00d3110b3c9b880a5d4a0e9e932e
Rohit Valiveti <rvv6@cornell.edu>
2023-05-02 18:17:51 -0400

    directory chanages

--- server/app.py
+++ server/app.py
@@ -1,5 +1,9 @@
 import json
-from flask import Flask, request
+from flask import Flask, session, request, redirect
+from flask_session import Session
+import sys
+sys.path.insert(0,"..")
+from server.SpotifyRecommender import SpotifyRecommender
 
 app = Flask(__name__)
 
@@ -13,3 +17,17 @@ def success_response(data, code=200):
 def failure_response(msg, code):
     return json.dumps({"error": msg}), code
 
+
+def auth():
+    pass
+
+# sp = SpotifyRecommender()
+
+# playlists = sp.playlists()
+
+# print(playlists)
+
+auth()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
