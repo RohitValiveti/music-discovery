b5d3cfcbdc140d59d5928ebfa586c0e87f9b4f97
Rohit Valiveti <rvv6@cornell.edu>
2023-07-02 11:17:01 -0400

    edit back button

--- client/src/pages/Recommendations.tsx
+++ client/src/pages/Recommendations.tsx
@@ -47,35 +47,37 @@ const Recommendations = () => {
 
   return (
     <div>
-      <Link
-        to={"/chooseplaylist"}
-        style={{
-          color: "#1db954",
-          textDecoration: "none",
-          border: "1px solid #1db954",
-          borderRadius: "4px",
-          padding: "8px",
-          marginBottom: "8px",
-          cursor: "pointer",
-          display: "inline-block",
-          marginRight: "10px",
-        }}
-      >
-        <FastRewindIcon
-          fontSize="large"
-          style={{
-            position: "absolute",
-            top: "10px",
-            left: "35px",
-            cursor: "pointer",
-          }}
-        />
-        <br></br>
-        <br></br>
-        <div>Back to playlists</div>
-      </Link>
       {tracks.length !== 0 ? (
         <div>
+          <Link
+            to={"/chooseplaylist"}
+            style={{
+              color: "#1db954",
+              textDecoration: "none",
+              border: "1px solid #1db954",
+              borderRadius: "4px",
+              padding: "8px",
+              marginBottom: "8px",
+              cursor: "pointer",
+              display: "inline-block",
+              marginRight: "10px",
+            }}
+          >
+            <FastRewindIcon
+              fontSize="large"
+              style={{
+                position: "absolute",
+                top: "10px",
+                left: "35px",
+                cursor: "pointer",
+              }}
+            />
+            <br></br>
+            <br></br>
+            <div>
+              <strong>Back to playlists</strong>
+            </div>
+          </Link>
           <div className="homepage" style={{ color: "#535353" }}>
             <Typography variant="h3" style={{ padding: 20 }}>
               Your Recommendations based on this Playlist
