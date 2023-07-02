c6fd077f63b1089f8e7bfbab6b3b38276274220c
Rohit Valiveti <rvv6@cornell.edu>
2023-07-02 19:12:23 -0400

    changed method types for playlist info

--- server/app.py
+++ server/app.py
@@ -228,8 +228,8 @@ def cover_from_track():
     return success_response(sp.track_cover(track_id))
 
 
-@app.route('/playlist-info')
-def get_playlist_cover():
+@app.route('/playlist-info/', methods=["GET", "POST"])
+def get_playlist_info():
     """
     Given the id of a playlist, returns the img url and name of it
     """
